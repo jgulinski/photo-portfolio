5ab49691c2b322f8dbc70595d4612f708e8be7a5
Jakub Gulinski <gulinskijakub@gmail.com>
2024-09-07 13:27:26 +0200

    add new background

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,22 @@ import { Tab, TabGroup, TabList, TabPanel, TabPanels } from "@headlessui/react";
 import Header from "./components/Header";
 import TabSection from "./components/TabSection";
 import Footer from "./components/Footer";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-[url('/background-black.JPG')] bg-cover bg-fixed text-white">
-      <div className="mx-auto flex flex-col min-h-screen max-w-screen-2xl backdrop-blur-sm backdrop-brightness-50">
+    <div className="min-h-screen text-white relative">
+      <div className="absolute inset-0 z-0">
+        <Image
+          src="/background.jpg"
+          alt="background"
+          layout="fill"
+          objectFit="cover"
+          quality={100}
+        />
+      </div>
+      <div className="absolute inset-0 bg-gradient-to-b from-black via-transparent to-black opacity-80 z-10"></div>
+      <div className="relative z-20 mx-auto flex flex-col min-h-screen max-w-screen-2xl">
         <Header />
         <main className="flex-grow">
           <TabSection />
